b0819efa42f60d780185a0efcb1452705f8242bd
Rudi Merlos Carrión <rudimerlos@gmail.com>
2022-08-15 14:48:07 +0200

    Check points and win status.

--- core/src/org/rmc/MainGame.java
+++ core/src/org/rmc/MainGame.java
@@ -13,7 +13,7 @@ public class MainGame extends BaseGame {
     public static final int SOUTH = 2;
     public static final int WEST = 3;
 
-    private static int numberMummies = 8;
+    private static int numberMummies = 2;
     private static int mummySpeed = 140;
     private static int mummyRange = 200;
 
--- core/src/org/rmc/screen/LevelScreen.java
+++ core/src/org/rmc/screen/LevelScreen.java
@@ -4,6 +4,7 @@ import org.rmc.MainGame;
 import org.rmc.entity.CrossPoint;
 import org.rmc.entity.Direction;
 import org.rmc.entity.Footprint;
+import org.rmc.entity.Goal;
 import org.rmc.entity.Mummy;
 import org.rmc.entity.Player;
 import org.rmc.entity.Solid;
@@ -25,10 +26,15 @@ import com.badlogic.gdx.math.MathUtils;
 public class LevelScreen extends BaseScreen {
 
     private Player player;
-    private Solid goal;
+    private Goal goal;
 
     private static final int BLOCK_EMPTY = 7;
     private static final int BLOCK_TREASURE = 10;
+
+    private int score;
+    private int lives;
+    private boolean key;
+    private boolean royal;
 
     @Override
     public void initialize() {
@@ -94,54 +100,21 @@ public class LevelScreen extends BaseScreen {
         }
 
         // mummy
-        MapProperties mummyLeft = tma.getRectangleList("mummy_start_left").get(0).getProperties();
-        MapProperties mummyRight = tma.getRectangleList("mummy_start_right").get(0).getProperties();
-        float xLeft = (float) mummyLeft.get("x");
-        float yLeft = (float) mummyLeft.get("y");
-        float xRight = (float) mummyRight.get("x");
-        float yRight = (float) mummyRight.get("y");
-        int mummyNumberLeft = 0;
-        int mummyNumberRight = 0;
-        for (int i = 0; i < MainGame.getNumberMummies(); i++) {
-            if (i % 2 == 0) {
-                if (mummyNumberLeft == 0) {
-                    new Mummy((float) mummyLeft.get("x"), (float) mummyLeft.get("y"),
-                            this.mainStage, this.player, MainGame.getMummySpeed(),
-                            MainGame.getMummyRange(), MainGame.EAST);
-                } else if (mummyNumberLeft % 2 != 0) {
-                    xLeft += 48;
-                    new Mummy(xLeft, (float) mummyLeft.get("y"), this.mainStage, this.player,
-                            MainGame.getMummySpeed(), MainGame.getMummyRange(), MainGame.EAST);
-                } else {
-                    yLeft += 48;
-                    new Mummy((float) mummyLeft.get("x"), yLeft, this.mainStage, this.player,
-                            MainGame.getMummySpeed(), MainGame.getMummyRange(), MainGame.NORTH);
-                }
-                mummyNumberLeft++;
-            } else {
-                if (mummyNumberRight == 0) {
-                    new Mummy((float) mummyRight.get("x"), (float) mummyRight.get("y"),
-                            this.mainStage, this.player, MainGame.getMummySpeed(),
-                            MainGame.getMummySpeed(), MainGame.WEST);
-                } else if (mummyNumberRight % 2 != 0) {
-                    xRight -= 48;
-                    new Mummy(xRight, (float) mummyRight.get("y"), this.mainStage, this.player,
-                            MainGame.getMummySpeed(), MainGame.getMummySpeed(), MainGame.WEST);
-                } else {
-                    yRight += 48;
-                    new Mummy((float) mummyRight.get("x"), yRight, this.mainStage, this.player,
-                            MainGame.getMummySpeed(), MainGame.getMummySpeed(), MainGame.NORTH);
-                }
-                mummyNumberRight++;
-            }
-        }
+        this.initializeMummies(tma);
 
         // goal
         MapObject goalObject = tma.getRectangleList("goal").get(0);
         MapProperties goalProperties = goalObject.getProperties();
-        this.goal = new Solid((float) goalProperties.get("x"), (float) goalProperties.get("y"),
+        this.goal = new Goal((float) goalProperties.get("x"), (float) goalProperties.get("y"),
                 (float) goalProperties.get("width"), (float) goalProperties.get("height"),
                 this.mainStage);
+
+        // TODO set score and lives with MainGame data
+        this.score = 0;
+        this.lives = 5;
+
+        this.key = false;
+        this.royal = false;
     }
 
     // create different types of blocks in random location
@@ -204,6 +177,50 @@ public class LevelScreen extends BaseScreen {
         return types;
     }
 
+    private void initializeMummies(TilemapActor tma) {
+        MapProperties mummyLeft = tma.getRectangleList("mummy_start_left").get(0).getProperties();
+        MapProperties mummyRight = tma.getRectangleList("mummy_start_right").get(0).getProperties();
+        float xLeft = (float) mummyLeft.get("x");
+        float yLeft = (float) mummyLeft.get("y");
+        float xRight = (float) mummyRight.get("x");
+        float yRight = (float) mummyRight.get("y");
+        int mummyNumberLeft = 0;
+        int mummyNumberRight = 0;
+        for (int i = 0; i < MainGame.getNumberMummies(); i++) {
+            if (i % 2 == 0) {
+                if (mummyNumberLeft == 0) {
+                    new Mummy((float) mummyLeft.get("x"), (float) mummyLeft.get("y"),
+                            this.mainStage, this.player, MainGame.getMummySpeed(),
+                            MainGame.getMummyRange(), MainGame.EAST);
+                } else if (mummyNumberLeft % 2 != 0) {
+                    xLeft += 48;
+                    new Mummy(xLeft, (float) mummyLeft.get("y"), this.mainStage, this.player,
+                            MainGame.getMummySpeed(), MainGame.getMummyRange(), MainGame.EAST);
+                } else {
+                    yLeft += 48;
+                    new Mummy((float) mummyLeft.get("x"), yLeft, this.mainStage, this.player,
+                            MainGame.getMummySpeed(), MainGame.getMummyRange(), MainGame.NORTH);
+                }
+                mummyNumberLeft++;
+            } else {
+                if (mummyNumberRight == 0) {
+                    new Mummy((float) mummyRight.get("x"), (float) mummyRight.get("y"),
+                            this.mainStage, this.player, MainGame.getMummySpeed(),
+                            MainGame.getMummySpeed(), MainGame.WEST);
+                } else if (mummyNumberRight % 2 != 0) {
+                    xRight -= 48;
+                    new Mummy(xRight, (float) mummyRight.get("y"), this.mainStage, this.player,
+                            MainGame.getMummySpeed(), MainGame.getMummySpeed(), MainGame.WEST);
+                } else {
+                    yRight += 48;
+                    new Mummy((float) mummyRight.get("x"), yRight, this.mainStage, this.player,
+                            MainGame.getMummySpeed(), MainGame.getMummySpeed(), MainGame.NORTH);
+                }
+                mummyNumberRight++;
+            }
+        }
+    }
+
     @Override
     public void update(float delta) {
         this.checkForBlocks();
@@ -225,9 +242,10 @@ public class LevelScreen extends BaseScreen {
             }
         }
 
-        if (this.player.overlaps(this.goal)) {
-            // TODO check if the player has got key and royal to pass to the next level
+        if (this.player.overlaps(this.goal) && this.key && this.royal) {
+            System.out.println("WIN!");
         }
+
     }
 
     private void checkForBlocks() {
@@ -243,8 +261,20 @@ public class LevelScreen extends BaseScreen {
                         block.setDiscovered(false);
                 }
 
-                if (block.isDiscovered())
+                if (block.isDiscovered()) {
                     block.setAnimationPaused(false);
+
+                    if (block instanceof BlockTreasure) {
+                        this.score += 5;
+                        System.out.println("Score: " + this.score);
+                    } else if (block instanceof BlockKey) {
+                        this.key = true;
+                        System.out.println("KEY");
+                    } else if (block instanceof BlockRoyal) {
+                        this.royal = true;
+                        System.out.println("ROYAL");
+                    }
+                }
             }
         }
     }
